0f425f5b0f1eea7242effce47ddb4d3492e6b4d1
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2024-07-27 17:35:25 +0530

    Cors added

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 import express from "express";
 import dbConnect from "./dbConnect.js";
-import 'dotenv/config'
-import userRouter from "./Routers/users.routers.js"
+import "dotenv/config";
+import userRouter from "./Routers/users.routers.js";
 import cookieParser from "cookie-parser";
-
+import cors from "cors";
 
 let server = express();
 
 let PORT = process.env.PORT || 3000;
 
 server.use(express.json()); // it is used to read the data of req-->body
-server.use(express.urlencoded({ extended: true })); // It will parse incoming data from url 
-server.use(cookieParser()); //this is used to read the cookie 
+server.use(express.urlencoded({ extended: true })); // It will parse incoming data from url
+server.use(cookieParser()); //this is used to read the cookie
+server.use(
+  cors({
+    origin: "http://localhost:5174",
+    credentials: true,
+  })
+);
 
-
-server.use("/users" , userRouter); 
-
+server.use("/users", userRouter);
 
 dbConnect()
   .then(() => {
